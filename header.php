1ee74b50d5f5c97937b14c799dae44bd567e91d3
Quentin Etourmy <q.etourmy@gmail.com>
2025-02-27 16:42:44 +0100

    modif

--- header.php
+++ header.php
@@ -20,12 +20,12 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
 
   <!-- font awesome style -->
-  <link href="/css/font-awesome.min.css" rel="stylesheet" />
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
 
   <!-- Custom styles for this template -->
-  <link href="/css/style.css" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet" />
   <!-- responsive style -->
-  <link href="/css/new.css" rel="stylesheet"/>
+  <link href="css/new.css" rel="stylesheet"/>
 </head>
 
 <body>
